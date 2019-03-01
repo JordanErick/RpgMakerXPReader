c8a4eb004a4f3c08e9815eb611007f909a1aecfc
Heaven31415 <Heaven31415@gmail.com>
2019-03-01 16:26:00 +0100

    Reorganize main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,133 @@
 #include "reader.hpp"
 
-#include <iostream>
-
-void print(const Any& any)
+void writeToString(std::string& str, const Any& any)
 {
     switch (any.type().toValue())
     {
-        case Type::Nil:
-            std::cout << "nil";
+        case Type::Array:
+            break;
+
+        case Type::Bignum:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
             break;
 
         case Type::Bool:
-            std::cout << (*any.as<bool>() ? "true" : "false");
+            break;
+
+        case Type::Class:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
+
+        case Type::Data:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
+
+        case Type::Extended:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
             break;
 
         case Type::Fixnum:
-            std::cout << *any.as<i32>();
+            break;
+
+        case Type::Float:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
+
+        case Type::Hash:
+            break;
+
+        case Type::HashDef:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
+
+        case Type::Ivar:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
+
+        case Type::Link:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
+
+        case Type::Module:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
+
+        case Type::ModuleOld:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
+
+        case Type::Nil:
+            break;
+
+        case Type::Object:
+            break;
+
+        case Type::Regexp:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
             break;
 
         case Type::String:
-        case Type::Symbol:
-        case Type::Symlink:
-            std::cout << '"' << *any.as<std::string>() << '"';
             break;
 
-        case Type::Array:
-        {
-            auto* array = any.as<Array>();
+        case Type::Struct:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
 
-            std::cout << '[';
+        case Type::Symbol:
+            break;
 
-            for (size_t i = 0; i < array->size(); i++)
-            {
-                print(array->at(i));
+        case Type::Symlink:
+            break;
 
-                if (i != array->size() - 1)
-                    std::cout << ' ';
-            }
+        case Type::Uclass:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
 
-            std::cout << "]";
+        case Type::Unknown:
+            throw std::runtime_error(fmt::format("Unknown type: {}", any.type()));
+            break;
 
-        } break;
+        case Type::UserDef:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
 
-    case Type::Object:
-    {
-        auto* object = any.as<Object>();
+        case Type::UserMarshal:
+            throw std::runtime_error(fmt::format("Not implemented: {}", any.type()));
+            break;
 
-        std::cout << "{\n";
-
-        for (const auto& pair : *object)
-        {
-            std::cout << pair.first;
-            std::cout << " -> ";
-            print(pair.second);
-            std::cout << "\n";
-        }
-        std::cout << "}\n";
-    } break;
-
-    case Type::Hash:
-    {
-        auto* hash = any.as<Hash>();
-
-        std::cout << "{\n";
-
-        for (const auto& pair : *hash)
-        {
-            print(pair.first);
-            std::cout << " -> ";
-            print(pair.second);
-            std::cout << "\n";
-        }
-        std::cout << "}\n";
-
-    } break;
-
-    case Type::UserDef:
-        std::cout << "userdef(" << any.as<Table>()->indices << ")";
-        break;
-
-    default:
-        std::cout << "Unknown";
-        break;
+        default:
+            throw std::runtime_error(fmt::format("Invalid type: {}", any.type()));
     }
 }
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        fmt::print("Usage: marshal_to_cpp filename\n");
+        fmt::print("Usage: marshal_to_cpp input_filename output_filename\n");
         return 1;
     }
 
-    Reader reader{ loadFileIntoMemory(argv[1]) };
-    Any any = reader.parse();
-    print(any);
+    const std::string inputFilename{ argv[1] };
+    const std::string outputFilename{ argv[2] };
+
+    try 
+    {
+        auto bytes = loadFileIntoMemory(inputFilename);
+
+        Reader reader{ bytes };
+        Any any = reader.parse();
+
+        std::string output = "hi";
+        writeToString(output, any);
+
+        writeStringToFile(outputFilename, output);
+    }
+    catch (const std::runtime_error& exception)
+    {
+        fmt::print("Error: {}", exception.what());
+        return 1;
+    }
+
+    return 0;
 }
