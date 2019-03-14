c4713d2de6cccb4a06f7c26fb656da8fb20ae9cd
Heaven31415 <Heaven31415@gmail.com>
2019-03-14 18:33:03 +0100

    Add SFML to engine.

--- premake5.lua
+++ premake5.lua
@@ -29,11 +29,15 @@ project "engine"
   removefiles "src/reader/main.cpp"
 
   includedirs "include"
+  includedirs "C:/SFML/install/include"
+  libdirs "C:/SFML/install/lib"
 
   filter "configurations:Debug"
     defines "DEBUG"
     symbols "On"
+    links {"sfml-audio-d", "sfml-graphics-d", "sfml-network-d", "sfml-system-d", "sfml-window-d"}
 
   filter "configurations:Release"
     defines "NDEBUG"
     optimize "On"
+    links {"sfml-audio", "sfml-graphics", "sfml-network", "sfml-system", "sfml-window"}
--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,8 +1,24 @@
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
 
+#include <SFML/Graphics.hpp>
+
 int main()
 {
-    fmt::print("Hello, World!\n");
+    sf::RenderWindow window{ sf::VideoMode{640, 480}, "Engine" };
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return EXIT_SUCCESS;
 }
